a0f328e6a464b27d465f27a46f8b8e9b4734d351
U-WangE <chamchamm@naver.com>
2021-12-06 08:26:03 +0900

    swtich sound 추가
    swtich sound 추가

--- ModelHouseSimulation/Assets/Switch.cs
+++ ModelHouseSimulation/Assets/Switch.cs
@@ -6,26 +6,36 @@ using System;
 public class Switch : MonoBehaviour
 {
     public GameObject light;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     void Update()
     {
-        try{
-            if(MouseClick.objectName.Equals("entrance_Light"))
+        try
+        {
+            if (MouseClick.objectName == gameObject.name)
             {
-                
-            }
-            else if(MouseClick.objectName == gameObject.name) {
-                if(light.activeInHierarchy)
+                if (light.activeInHierarchy)
                 {
+                    if (!MouseClick.objectName.Equals("LightTable06_1On"))
+                        audioSource.Play();
                     light.SetActive(false);
                 }
                 else
                 {
+                    if (!MouseClick.objectName.Equals("LightTable06_1On"))
+                        audioSource.Play();
                     light.SetActive(true);
                 }
                 MouseClick.objectName = null;
             }
-        }catch(Exception ex){
+        }
+        catch (Exception ex)
+        {
             Debug.Log("Switch : " + ex);
         }
     }
